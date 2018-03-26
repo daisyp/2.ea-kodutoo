195ba2d65ffc03adf15a8122b66abb138456be06
Kurmo Rootsi <kurmo.rootsi@hotmail.com>
2018-03-26 14:52:14 +0300

    added game restart, highscores, and localstorage

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const TYPER = function () {
   this.gameScore = 0
   this.gameMultiplier = 1
   this.missType = 0
+  this.gamePoint = 0
 
   this.init()
 }
@@ -24,76 +25,96 @@ const TYPER = function () {
 window.TYPER = TYPER
 
 TYPER.prototype = {
-  init: function () {
-      
-    this.canvas = document.getElementsByTagName('canvas')[0]
-    this.ctx = this.canvas.getContext('2d')
-    var playerName = document.getElementById("playerName").value;
-    console.log("tere " + playerName)
+    init: function () {
 
-    this.canvas.style.width = this.WIDTH + 'px'
-    this.canvas.style.height = this.HEIGHT + 'px'
+        this.canvas = document.getElementsByTagName('canvas')[0]
+        this.ctx = this.canvas.getContext('2d')
+        var playerName = document.getElementById("playerName").value;
+        console.log("tere " + playerName)
 
-    this.canvas.width = this.WIDTH * 2
-    this.canvas.height = this.HEIGHT * 2
-    
-    this.loadWords()
-  },
+        this.canvas.style.width = this.WIDTH + 'px'
+        this.canvas.style.height = this.HEIGHT + 'px'
 
-  loadWords: function () {
-    const xmlhttp = new XMLHttpRequest()
+        this.canvas.width = this.WIDTH * 2
+        this.canvas.height = this.HEIGHT * 2
 
-    xmlhttp.onreadystatechange = function () {
-      if (xmlhttp.readyState === 4 && (xmlhttp.status === 200 || xmlhttp.status === 0)) {
-        const response = xmlhttp.responseText
-        const wordsFromFile = response.split('\n')
+        this.loadWords()
+    },
 
-        typer.words = structureArrayByWordLength(wordsFromFile)
+    loadWords: function () {
+        const xmlhttp = new XMLHttpRequest()
 
-        typer.start()
-      }
-    }
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState === 4 && (xmlhttp.status === 200 || xmlhttp.status === 0)) {
+                const response = xmlhttp.responseText
+                const wordsFromFile = response.split('\n')
 
-    xmlhttp.open('GET', './lemmad2013.txt', true)
-    xmlhttp.send()
-  },
+                typer.words = structureArrayByWordLength(wordsFromFile)
 
-  start: function () {
-    this.generateWord()
-    this.word.Draw()
+                typer.start()
+            }
+        }
 
-    window.addEventListener('keypress', this.keyPressed.bind(this))
-  },
+        xmlhttp.open('GET', './lemmad2013.txt', true)
+        xmlhttp.send()
+    },
 
-  generateWord: function () {
-    const generatedWordLength = this.wordMinLength + parseInt(this.gameScore / 5)
-    const randomIndex = (Math.random() * (this.words[generatedWordLength].length - 1)).toFixed()
-    const wordFromArray = this.words[generatedWordLength][randomIndex]
-
-    this.word = new Word(wordFromArray, this.canvas, this.ctx)
-  },
-
-  keyPressed: function (event) {
-    const letter = String.fromCharCode(event.which)
-
-    if (letter === this.word.left.charAt(0)) {
-      this.word.removeFirstLetter()
-
-      if (this.word.left.length === 0) {
-        this.gameScore = 1 * this.gameMultiplier
-        this.gameMultiplier += 0.2
+    start: function () {
         this.generateWord()
-      }
-      console.log("nono " + playerName)
-      this.word.Draw()
-    } else {
-        this.missType += 1
+        this.word.Draw()
+
+        window.addEventListener('keypress', this.keyPressed.bind(this))
+    },
+
+    generateWord: function () {
+        const generatedWordLength = this.wordMinLength + parseInt(this.gameScore / 5)
+        const randomIndex = (Math.random() * (this.words[generatedWordLength].length - 1)).toFixed()
+        const wordFromArray = this.words[generatedWordLength][randomIndex]
+
+        this.word = new Word(wordFromArray, this.canvas, this.ctx)
+    },
+
+    keyPressed: function (event) {
+        const letter = String.fromCharCode(event.which)
+
+        if (letter === this.word.left.charAt(0)) {
+            this.word.removeFirstLetter()
+
+            if (this.word.left.length === 0) {
+                this.gameScore = 1 * this.gameMultiplier
+                this.gameMultiplier += 0.2
+                this.generateWord()
+            }
+            this.word.Draw()
+        } else {
+            this.missType += 1;
+        }
+        if (this.missType >= 5) {
+            this.gameMultiplier = 1;
+            this.missType = 0;
+            this.gamePoint += 1;
+        }
+        if (this.gamePoint >= 3) {
+            score = []
+            if (window.localStorage.length == 0) {
+                score.push([document.getElementById("playerName").value,Math.round(this.gameScore, 1)])
+                localStorage.setItem('score', JSON.stringify(score));
+            } else {
+                let local = JSON.parse(localStorage.getItem('score'));
+                local.push([document.getElementById("playerName").value,Math.round(this.gameScore, 1)])
+                localStorage.setItem("score", JSON.stringify(local));
+            }
+            this.gamePoint = 0
+            this.restart = confirm("Restart game?")
+            if(this.restart == true){
+                this.gameScore = 0
+                this.gameMultiplier = 1
+                this.missType = 0
+                this.gamePoint = 0
+                restartGame()
+            }
+        }
     }
-    if (this.missType >= 5){
-        this.gameMultiplier = 1;
-        this.missType = 0;
-    }
-  }
 }
 
 /* WORD */
@@ -111,13 +132,6 @@ Word.prototype = {
     this.ctx.textAlign = 'center'
     this.ctx.font = '140px Courier'
     this.ctx.fillText(this.left, this.canvas.width / 2, this.canvas.height / 2)
-    /* var gameover = false;
-    (see kus score siis if statementiga gameover = true)
-    if(gameover) {
-        this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
-        text("GAME OVER", "75px Amatic SC", 300, 400, "black");
-    } */
-      
   },
 
   removeFirstLetter: function () {
@@ -143,6 +157,14 @@ window.onload = function () {
   const typer = new TYPER()
   window.typer = typer
 }
+
+function restartGame(){
+    const typer = new TYPER()
+    window.typer = typer
+    typer.generateWord()
+    typer.word.Draw()
+}
+
 function checkNameInput() {
     let x = document.getElementById("playerName").value;
     if (document.getElementById("playerName").value != ""){
@@ -154,4 +176,4 @@ function checkNameInput() {
     }
 }
 
-document.getElementById('sona').innerHTML = this.gameScore;
+/*document.getElementById('sona').innerHTML = this.gameScore;*/
